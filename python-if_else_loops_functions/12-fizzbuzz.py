abe5b0b2fa830adc6b5badcf22f59ae022f4c852
electrikbox <electrikbox@icloud.com>
2023-09-25 17:42:04 +0200

    fizzbuzz

---
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+def fizzbuzz():
+    sentence = ""
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            sentence += "FizzBuzz "
+        elif i % 3 == 0:
+            sentence += "Fizz "
+        elif i % 5 == 0:
+            sentence += "Buzz "
+        else:
+            sentence += str(i) + " "
+
+    print(sentence)
+
+
+fizzbuzz()
+print("")
