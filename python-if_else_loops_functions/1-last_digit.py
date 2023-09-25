8db2cc95f05b883b7af01affd3a862b183cfca97
electrikbox <electrikbox@icloud.com>
2023-09-25 13:44:16 +0200

    remove 'sign' var

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -2,10 +2,9 @@
 import random
 number = random.randint(-10000, 10000)
 last_digit = abs(number) % 10
-sign = ""
 
 if number < 0:
-    sign = "-"
+    last_digit = -last_digit
 
 if last_digit > 5:
     sentence = "greater than 5"
@@ -14,4 +13,4 @@ elif last_digit == 0:
 else:
     sentence = "less than 6 and not 0"
 
-print(f"Last digit of {number} is {sign}{last_digit} and is {sentence}")
+print(f"Last digit of {number} is {last_digit} and is {sentence}")
