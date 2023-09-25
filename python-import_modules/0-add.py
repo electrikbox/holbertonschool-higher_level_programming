fef47c75cd8f28e89e8f1c0067ccbdc2a121e516
electrikbox <electrikbox@icloud.com>
2023-09-25 18:40:53 +0200

    add condition

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
-import add_0 as my_module
-
-a = 1
-b = 2
-
-result = my_module.add(a, b)
-
-print("{} + {} = {}".format(a, b, result))
+if __name__ == "__main__":
+    from add_0 import add
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
