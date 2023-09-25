7e914f6a115e1f45e729b69d12563eae7edf931e
electrikbox <electrikbox@icloud.com>
2023-09-25 15:58:51 +0200

    change to only return a ** b

--- python-if_else_loops_functions/11-pow.py
+++ python-if_else_loops_functions/11-pow.py
@@ -1,17 +1,4 @@
 #!/usr/bin/python3
 
 def pow(a, b):
-    resultat = 1
-
-    if a == 0:
-        return 0
-
-    if b < 0:
-        a = 1 / a
-        b = -b
-        resultat = 1.0
-
-    for _ in range(b):
-        resultat *= a
-
-    return resultat
+    return a ** b
