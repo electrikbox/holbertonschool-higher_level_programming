d3d2525df8e4e14232ed811c562a9d725bf1c25a
electrikbox <electrikbox@icloud.com>
2023-09-25 14:02:23 +0200

    use a %c in print function

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,9 +1,3 @@
 #!/usr/bin/python3
-alphabet = ""
-ascii_a = ord('a')
-ascii_z = ord('z')
-
-for ascii_value in range(ascii_a, ascii_z + 1):
-    alphabet += chr(ascii_value)
-
-print(f"{alphabet}", end="")
+for ascii_value in range(97, 122):
+    print('%c' % ascii_value, end='')
