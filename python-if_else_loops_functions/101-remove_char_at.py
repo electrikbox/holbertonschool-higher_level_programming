8d6c3a7ca22e59d3d307010d87a4ad7e6d1d56ed
electrikbox <electrikbox@icloud.com>
2023-09-26 13:57:35 +0200

    change first line

--- python-if_else_loops_functions/101-remove_char_at.py
+++ python-if_else_loops_functions/101-remove_char_at.py
@@ -1,7 +1,6 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 
 def remove_char_at(str, n):
-    if n >= 0:
-        return str[:n] + str[n+1:]
-    else:
+    if n < 0:
         return str
+    return str[:n] + str[n+1:]
