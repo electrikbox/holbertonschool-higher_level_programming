9a0417f149ce7a296c662165fed97400faf2a2dd
electrikbox <electrikbox@icloud.com>
2023-09-26 13:09:53 +0200

    new try with first code

--- python-if_else_loops_functions/101-remove_char_at.py
+++ python-if_else_loops_functions/101-remove_char_at.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
-
 def remove_char_at(str, n):
-    if n < 0:
-        return str
-    return (str[:n] + str[n + 1:])
+    sentence = ""
+    for i in range(len(str)):
+        if i != n:
+            sentence += str[i]
+    return sentence
