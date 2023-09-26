f7a0a6cb4f186cad4178782c4970d7f6253f8e84
electrikbox <electrikbox@icloud.com>
2023-09-26 12:40:59 +0200

    add condition, change the way to return

--- python-if_else_loops_functions/101-remove_char_at.py
+++ python-if_else_loops_functions/101-remove_char_at.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 def remove_char_at(str, n):
-    sentence = ""
-    for i in str:
-        if str.index(i) != n:
-            sentence += i
-    return sentence
+    if n >= 0 and n < len(str):
+        return str[:n] + str[n+1:]
+    else:
+        return str
