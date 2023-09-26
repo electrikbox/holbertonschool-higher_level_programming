5458a26931555f1831c3da4ea2765866817f94ea
electrikbox <electrikbox@icloud.com>
2023-09-26 10:44:15 +0200

    make the code way more simple

--- python-import_modules/5-variable_load.py
+++ python-import_modules/5-variable_load.py
@@ -1,12 +1,4 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import variable_load_5
-
-    module_attributes = dir(variable_load_5)
-
-    for attribute_name in module_attributes:
-        if not attribute_name.startswith("__"):
-            attribute_value = getattr(variable_load_5, attribute_name)
-            if callable(attribute_value):
-                continue
-            print(attribute_value)
+    from variable_load_5 import a
+    print(a)
