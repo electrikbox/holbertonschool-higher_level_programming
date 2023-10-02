ddd8d8d91c92ece36ed903326b0dca898174dcef
electrikbox <electrikbox@icloud.com>
2023-10-02 14:29:24 +0200

    add null check

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 
 def print_reversed_list_integer(my_list=[]):
+    if my_list == None:
+        my_list = []
+        
     my_list.reverse()
-    [print("{:d}".format(i)) for i in my_list]
+    [print("{:d}".format(i)) for i in my_list[::-1]]
