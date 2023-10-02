d2b9b95abe90d9c9c91bd0d2ab3c4234b7fb29b5
electrikbox <electrikbox@icloud.com>
2023-10-02 14:32:11 +0200

    remove 1 reverse handle

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -5,4 +5,4 @@ def print_reversed_list_integer(my_list=[]):
         my_list = []
 
     my_list.reverse()
-    [print("{:d}".format(i)) for i in my_list[::-1]]
+    [print("{:d}".format(i)) for i in my_list]
