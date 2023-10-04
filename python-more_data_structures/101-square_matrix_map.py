0c10d8b86f744ed9744bd93fdd611ae5fae3dc97
electrikbox <electrikbox@icloud.com>
2023-10-04 22:05:19 +0200

    remove com

--- python-more_data_structures/101-square_matrix_map.py
+++ python-more_data_structures/101-square_matrix_map.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 
 def square_matrix_map(matrix=[]):
-    # return [[i ** 2 for i in row] for row in matrix]
     return list(map(lambda row: list(map(lambda x: x ** 2, row)), matrix))
