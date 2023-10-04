13ecebc0267e4cc40d1e2bd2eae44ffd977cc717
electrikbox <electrikbox@icloud.com>
2023-10-04 16:26:47 +0200

    reduce code len

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,13 +1,7 @@
 #!/usr/bin/python3
 
 def best_score(a_dictionary):
-    if not isinstance(a_dictionary, dict):
-        return None
-
-    winner = None
-
-    for key, value in a_dictionary.items():
-        if winner is None or value > a_dictionary[winner]:
-            winner = key
-
-    return winner
+    if a_dictionary:
+        return (max(a_dictionary, key=a_dictionary.get))
+    else:
+        return (None)
