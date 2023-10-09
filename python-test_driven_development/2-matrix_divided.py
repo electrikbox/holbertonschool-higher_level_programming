397f245753d5b52d5e22e5ba66fc88992473f475
electrikbox <electrikbox@icloud.com>
2023-10-09 19:16:23 +0200

    remove error msg variable

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -15,22 +15,22 @@ def matrix_divided(matrix, div):
         div (int, float): divisor
 
     Raises:
-        TypeError: if matrix is not a list, or empty list, or if matrix
-            contains non-int or non-float elements, or if matrix contains
-            rows of different sizes, or if div is not an int or float.
+        TypeError: if matrix is not a list, or empty list
+        TypeError: if matrix contains non-int or non-float elements
+        TypeError: if matrix contains rows of different sizes
+        TypeError: if div is not an int or float.
         ZeroDivisionError: if div is 0.
 
     Returns:
         list: list of int or float divided by divisor
     """
-    error_msg = "matrix must be a matrix (list of lists) of integers/floats"
 
     if (
         not isinstance(matrix, list) or matrix == [] or
         not all(isinstance(row, list) for row in matrix) or
         not all(isinstance(num, (int, float)) for row in matrix for num in row)
     ):
-        raise TypeError(error_msg)
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
 
     if not all(len(row) == len(matrix[0]) for row in matrix):
         raise TypeError("Each row of the matrix must have the same size")
