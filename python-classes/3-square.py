6a3eb633c07b9a931edc77f2d051717305985c4b
electrikbox <electrikbox@icloud.com>
2023-10-09 10:35:42 +0200

    create method that return square aera

---
+++ python-classes/3-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+"""class Square that defines a square"""
+
+
+class Square:
+    """ Square class that define a square with is size"""
+
+    def __init__(self, size=0):
+        """Instantiate a square with optional size
+
+        Args:
+            size (int, optional): Size of the square. Defaults to 0.
+
+        Raises:
+            TypeError: if size is not an integer
+            ValueError: if size is under 0
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Return the current square area."""
+        return self.__size ** 2
