8b20b59af565b288d1584314e372a1179fb543bb
electrikbox <electrikbox@icloud.com>
2023-10-09 14:36:14 +0200

    update docstring

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,21 +1,15 @@
 #!/usr/bin/python3
-""" Function that adds 2 integers. """
+"""
+
+Function that adds 2 integers.
+
+"""
 
 
 def add_integer(a, b=98):
-    """function to add 2 integer
-
-    Args:
-        a (int): first value
-        b (int, optional): second value. Defaults to 98.
-
-    Raises:
-        TypeError: if a is not an int
-        TypeError: if b is not an int
-
-    Returns:
-        int: sum a + b
-    """
+    """a (int): first value
+    b (int, optional): second value. Defaults to 98.
+    Returns: sum a + b"""
 
     if not isinstance(a, (int, float)):
         raise TypeError('a must be an integer')
