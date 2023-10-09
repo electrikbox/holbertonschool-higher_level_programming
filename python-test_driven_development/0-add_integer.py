c02265f651dcfa3b83932d98380feafe86691915
electrikbox <electrikbox@icloud.com>
2023-10-09 14:49:51 +0200

    cast value before checking

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -15,10 +15,13 @@ def add_integer(a, b=98):
         sum (int) a + b
     """
 
-    if not isinstance(a, (int, float)):
+    a_int = int(a)
+    b_int = int(b)
+
+    if not isinstance(a_int, int):
         raise TypeError('a must be an integer')
 
-    if not isinstance(b, (int, float)):
+    if not isinstance(b_int, int):
         raise TypeError('b must be an integer')
 
-    return int(a) + int(b)
+    return a_int + b_int
