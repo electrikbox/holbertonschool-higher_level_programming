7f783ee76445febe3d6163cdc21e12803ad4f479
electrikbox <electrikbox@icloud.com>
2023-10-16 17:33:52 +0200

    class raise exception

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+class that return a raise Exception
+"""
+
+
+class BaseGeometry:
+    """
+    class that return a raise Exception
+    """
+    def area(self):
+        raise Exception(f"{self.area.__name__} is not implemented")
