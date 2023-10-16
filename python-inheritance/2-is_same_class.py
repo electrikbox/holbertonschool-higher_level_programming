1a22cc269bc852eb7fb78798b86402f0f71dd2c1
electrikbox <electrikbox@icloud.com>
2023-10-16 16:21:33 +0200

    check if instance of class

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""function that return if instance is part of the class"""
+
+
+def is_same_class(obj, a_class):
+    """check if is instance of class
+
+    Args:
+        obj (any): object to check
+        a_class (any): given class
+
+    Returns:
+        bool: True if instance of class, False if not
+    """
+    return isinstance(obj, a_class)
