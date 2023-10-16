090cf889f695cb135ad18fb011bbfb9285a16552
electrikbox <electrikbox@icloud.com>
2023-10-16 15:43:25 +0200

    fix doc

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
+""" Defines an object attribute lookup function """
+
+
 def lookup(obj):
-    """
-    Returns the list of available attributes and methods of an object
-    """
+    """ Returns list of available attributes and methods of an object
+    Args:
+        obj: object to look up
+    Returns:
+        list of attributes and methods
+     """
 
     return dir(obj)
