7b969af44026a4b8a342280262da3b5c1635d2c9
electrikbox <electrikbox@icloud.com>
2023-10-16 17:56:24 +0200

    change != to 'is not'

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -16,7 +16,7 @@ class BaseGeometry:
             Exception: if function is not implemented
         """
         raise Exception(f"{self.area.__name__}() is not implemented")
-    
+
     def integer_validator(self, name, value):
         """
         value type validator
@@ -29,8 +29,7 @@ class BaseGeometry:
             TypeError: if value is not an int
             ValueError: if value is < 0
         """
-        if type(value) != int:
+        if type(value) is not int:
             raise TypeError(f"{name} must be an integer")
         if value < 0:
             raise ValueError(f"{name} must be greater than 0")
-
