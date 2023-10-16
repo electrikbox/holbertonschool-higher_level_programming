a581daacdd5ca5c5e112fd068ab42c82e2f6c5cc
electrikbox <electrikbox@icloud.com>
2023-10-16 16:14:07 +0200

    print sorted list

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+class with method that print sorted list
+"""
+
+
+class MyList(list):
+    """
+    class that print sorted list
+
+    Args:
+        list (list): list to sorted
+    """
+    def print_sorted(self):
+        """
+        print sorted list
+        """
+        if issubclass(MyList, list):
+            print(sorted(self))
