a919a234673229a937af0fea0700b22560cbb64b
electrikbox <electrikbox@icloud.com>
2023-10-16 17:35:20 +0200

    add '()' in raise message

--- python-inheritance/6-base_geometry.py
+++ python-inheritance/6-base_geometry.py
@@ -9,4 +9,4 @@ class BaseGeometry:
     class that return a raise Exception
     """
     def area(self):
-        raise Exception(f"{self.area.__name__} is not implemented")
+        raise Exception(f"{self.area.__name__}() is not implemented")
