6f1b11ff1062c37294dad5f38bb0001958e7dfc6
electrikbox <electrikbox@icloud.com>
2023-10-16 16:37:49 +0200

    check if is instance

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def is_kind_of_class(obj, a_class):
+    """check if obj is instance of class
+
+    Args:
+        obj (any): object t check
+        a_class (any): given class
+
+    Returns:
+        bool: True if is instance, False if not
+    """
+    return isinstance(obj, a_class)
