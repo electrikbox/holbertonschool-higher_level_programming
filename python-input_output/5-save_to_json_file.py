0460f44b7980a240e442de909383002e931f90ae
electrikbox <electrikbox@icloud.com>
2023-10-17 13:42:20 +0200

    add file in write change mode to write

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -15,5 +15,5 @@ def save_to_json_file(my_obj, filename):
 
     json_obj = json.dumps(my_obj)
 
-    with open(filename, "a", encoding="utf-8") as f:
+    with open(filename, "w", encoding="utf-8") as f:
         return f.write(json_obj)
