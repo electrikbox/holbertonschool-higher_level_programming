4a75bc3a669f452ad826d050d3ab7b273c5a55d8
electrikbox <electrikbox@icloud.com>
2023-10-17 14:01:24 +0200

    dump instead of write

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -13,6 +13,5 @@ def save_to_json_file(my_obj, filename):
         int: Number of characters written to the file
     """
 
-
     with open(filename, "w", encoding="utf-8") as f:
-        return json.dumps(my_obj, f)
+        json.dump(my_obj, f)
