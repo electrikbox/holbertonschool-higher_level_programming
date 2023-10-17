38c302069121405a2b6ee1694ddd18fe7414e92c
electrikbox <electrikbox@icloud.com>
2023-10-17 10:41:10 +0200

    dumps json

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+import json
+"""returns the JSON representation of an object"""
+
+
+def to_json_string(my_obj):
+    """function that returns the JSON representation of an object"""
+    return json.dumps(my_obj)
