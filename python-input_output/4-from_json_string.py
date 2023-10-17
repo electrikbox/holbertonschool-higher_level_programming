499b0f3c4d6af33df9b3fedff02852e861c5e07c
electrikbox <electrikbox@icloud.com>
2023-10-17 13:03:36 +0200

    add newlines

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -3,9 +3,11 @@
 
 import json
 
+
 def from_json_string(my_str):
     """function that returns an object represented by a JSON string
     Args:
         my_str (str): string to load
     """
+
     json.loads(my_str)
