20d1560b8f5ebaa86a0540b2c33c0e559954df8e
electrikbox <electrikbox@icloud.com>
2023-10-17 13:50:31 +0200

    update return statement

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -8,12 +8,11 @@ def save_to_json_file(my_obj, filename):
     """save json in a file
     Args:
         my_obj (any): object to save
-        filename (str): file where obj is save
+        filename (str): file where obj is saved
     Returns:
-        json: write file
+        int: Number of characters written to the file
     """
 
-    json_obj = json.dumps(my_obj)
 
     with open(filename, "w", encoding="utf-8") as f:
-        return f.write(json_obj)
+        return json.dumps(my_obj, f)
