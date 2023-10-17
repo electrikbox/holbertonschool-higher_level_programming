4c1692cc7f3a06cf73384a203386fbcdc5186424
electrikbox <electrikbox@icloud.com>
2023-10-17 20:29:28 +0200

    reload from json

---
+++ python-input_output/11-student.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""class Student"""
+
+
+class Student:
+    """Student class"""
+
+    def __init__(self, first_name, last_name, age):
+        """init Student
+        Args:
+            first_name (str): Student first name
+            last_name (str): Student last name
+            age (int): Student age
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """return Student dict
+        Args:
+            attrs (list, optional): Student attributs. Defaults to None.
+        Returns:
+            dict: Student dict
+        """
+        if attrs is None:
+            return self.__dict__
+        if all(isinstance(attr, str) for attr in attrs):
+            return {k: v for k, v in self.__dict__.items() if k in attrs}
+        
+    def reload_from_json(self, json):
+        for key, value in json.items():
+            setattr(self, key, value)
