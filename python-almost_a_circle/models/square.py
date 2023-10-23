8bc3a487afbab19995c64df2ff4f1cef2b05252d
electrikbox <electrikbox@icloud.com>
2023-10-23 23:04:50 +0200

    square class

---
+++ python-almost_a_circle/models/square.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+represent a square class
+"""
+
+
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """Square class
+    Args:
+        Rectangle (Rectangle): Mother class
+    """
+    def __init__(self, size, x=0, y=0, id=None):
+        """initialize square
+        Args:
+            size (int): square side size
+            x (int, optional): x position. Defaults to 0.
+            y (int, optional): y position. Defaults to 0.
+            id (any, optional): square id. Defaults to None.
+        """
+        super().__init__(size, size, x, y, id)
+        self.size = size
+
+    def __str__(self):
+        """print representation
+        Returns:
+            str: square representation
+        """
+        return "[{}] ({}) {}/{} - {}".format(
+            self.__class__.__name__, self.id, self.x, self.y, self.size
+            )
