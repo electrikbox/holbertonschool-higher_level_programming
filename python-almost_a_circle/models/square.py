21363fb574c9133dd74d540550795251170b18a3
electrikbox <electrikbox@icloud.com>
2023-10-23 23:16:19 +0200

    update setter

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -26,14 +26,15 @@ class Square(Rectangle):
     @property
     def size(self):
         return self.__size
-    
+
     @size.setter
     def size(self, value):
         if type(value) is not int:
             raise TypeError("width must be an integer")
         if value <= 0:
             raise ValueError("width must be > 0")
-        self.__size = value
+        self.__width = value
+        self.__height = value
 
     def __str__(self):
         """print representation
