f3ecada199b8ee041dab9a1ab6eb963231659015
electrikbox <electrikbox@icloud.com>
2023-10-24 10:17:43 +0200

    add static method to json

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -4,6 +4,8 @@ represent a base class that will help for future
 rectangle and square classes
 """
 
+import json
+
 
 class Base:
     """Base class"""
@@ -20,3 +22,10 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """ List to json string """
+        if list_dictionaries is None or list_dictionaries == "[]":
+            return "[]"
+        return json.dumps(list_dictionaries)
