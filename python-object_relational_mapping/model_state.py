02e3abecefa61001447fb6bf2b8c4a7ece15d11f
electrikbox <electrikbox@icloud.com>
2023-11-27 14:07:55 +0100

    add unique

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -9,10 +9,12 @@ Base = declarative_base()
 
 
 class State(Base):
+    """State class"""
     __tablename__ = "states"
 
     id = Column(
         Integer,
+        unique = True,
         nullable=False,
         primary_key=True,
         autoincrement=True
