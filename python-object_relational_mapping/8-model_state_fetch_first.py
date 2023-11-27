39813475f1e4f8a37e27ba68f974b87c01218955
electrikbox <electrikbox@icloud.com>
2023-11-27 15:36:07 +0100

    add order by

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -22,7 +22,7 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    state = session.query(State).first()
+    state = session.query(State).order_by(State.id).first()
 
     session.close()
 
