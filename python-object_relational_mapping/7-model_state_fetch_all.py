35409f3e243743ad5a77e549cee8008757e47092
electrikbox <electrikbox@icloud.com>
2023-11-27 15:25:04 +0100

    query using Alchemy session

---
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" script that lists all State objects from the database """
+
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import sys
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        username,
+        password,
+        database
+    ), pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    query = session.query(State).order_by(State.id).all()
+
+    session.close()
+
+    for state in query:
+        print("{}: {}".format(state.id, state.name))
