adfc430982daff10eb7ac472158b6027993b6672
electrikbox <electrikbox@icloud.com>
2023-11-27 15:34:13 +0100

    prints the first State object from the database

---
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+""" script that prints the first State object from the database """
+
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import sys
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        username,
+        password,
+        database
+    ), pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    state = session.query(State).first()
+
+    session.close()
+
+    if state:
+        print("{}: {}".format(state.id, state.name))
+    else:
+        print()
